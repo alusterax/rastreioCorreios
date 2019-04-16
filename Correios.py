7d094072804801a0e5f665886b14eec2f86f8a2b
alusterax <38334986+alusterax@users.noreply.github.com>
2019-04-16 20:44:33 -0300

    Update Correios.py

--- Correios.py
+++ Correios.py
@@ -1,45 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import urllib3
 import json
-
-
-# In[ ]:
-
 
 def rastrear(codigo):
     http = urllib3.PoolManager()
     r = http.request('GET', 'https://linketrack.com/'+codigo+'/json')
     return r.data
 
-
-# In[ ]:
-
-
 def mostrar(a):
     print('Mais recente primeiro:')
     for evento in a['eventos']:
         print('----------------\n'+evento['data']+'\n'+evento['hora']+'\n'+evento['local']+'\n'+evento['status'])
 
-
-# In[ ]:
-
-
 inp = input('\nDigite o código de rastreamento:\n')
-
-
-# In[ ]:
-
 
 dados = json.loads(rastrear(inp))
 
-
-# In[ ]:
-
-
 mostrar(dados)
-
